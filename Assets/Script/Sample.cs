c577d0d3613044303dd570c9536b9a74fc7a8939
戸澤海智 <st37071556@icloud.com>
2022-05-20 15:59:26 +0900

    メソッドにしたい部分を選んだ状態でコントロール+ｒ+ｍでメソッドにできる

--- Assets/Script/Sample.cs
+++ Assets/Script/Sample.cs
@@ -5,11 +5,11 @@ public class Sample : MonoBehaviour
 {
     private GameObject[] _cells;
     private int _selectedIndex = 0;
-
+    [SerializeField] uint _count = 0;
     private void Start()
     {
-        _cells = new GameObject[5];
-        for (var i = 0; i < 5; i++)
+        _cells = new GameObject[_count];
+        for (var i = 0; i < _cells.Length; i++)
         {
             var obj = new GameObject($"Cell{i}");
             obj.transform.parent = transform;
@@ -22,26 +22,40 @@ public class Sample : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow)) // 左キーを押した
         {
+
             _selectedIndex--;
         }
         if (Input.GetKeyDown(KeyCode.RightArrow)) // 右キーを押した
         {
             _selectedIndex++;
         }
+        if (Input.GetButtonDown("Jump"))
+        {
+            var cell = _cells[_selectedIndex];
+            Destroy(cell);
+            //_cells[_selectedIndex].SetActive(false);
+        }
 
-        for (var i = 0; i < 5; i++)
+        OnselectedChanged();
+    }
+
+    private void OnselectedChanged()
+    {
+        for (var i = 0; i < _count; i++)
         {
             var cell = _cells[i]; ;
+            if (!cell) { continue; }
+
             var image = cell.GetComponent<Image>();
 
             if (i == _selectedIndex) { image.color = Color.red; }
             else { image.color = Color.white; }
         }
-        if(_selectedIndex>4)
+        if (_selectedIndex > _cells.Length - 1)
         {
-            _selectedIndex = 4;
+            _selectedIndex = _cells.Length - 1;
         }
-        else if(_selectedIndex<0)
+        else if (_selectedIndex < 0)
         {
             _selectedIndex = 0;
         }
